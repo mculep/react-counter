b677411884b44ba9db06d406cfa8c75410310a39
mculep <mculep@gmail.com>
2021-01-24 14:30:27 -0800

    add react counter app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
 
 function App() {
+
+  const [count, setCount] = useState(0)
+
+  const incrementBtn = () => {
+    setCount(prevCount => prevCount + 1)
+  }
+
+  const decrementBtn = () => {
+    setCount(prevCount => prevCount - 1)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <button class="btn" onClick={incrementBtn}>Increment</button>
+      <button class="btn" onClick={decrementBtn}>Decrement</button>
     </div>
   );
 }
